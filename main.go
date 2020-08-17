50fa59adbc39c8981211d270f843f75feb94716a
Pedro Gracia <pedro.gracia@impulzia.com>
2020-08-17 11:14:35 +0200

    First version. Wrapper needs httpie.

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+// Simple wkhtmltopdf as service
+// TODO: tests
+
+import (
+	"fmt"
+	"log"
+	"log/syslog"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+var wkCmd string
+
+func main() {
+	// enable syslog
+	syslogger, err := syslog.New(syslog.LOG_INFO, "wkservice")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.SetOutput(syslogger)
+
+	// check wkhtmltopdf command
+	wkCmd = checkWkExists()
+
+	// set endpoiunt
+	http.HandleFunc("/", LaunchCmd)
+
+	// launch service
+	log.Println("Server listeng at :8080")
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func LaunchCmd(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	params := query.Get("params")
+	// check pipe
+	if strings.Contains(params, "|") {
+		log.Println("Aborting command. Pipe detected.")
+		return
+	}
+
+	fmt.Printf("Launching wkhtmltopdf with params: '%s'", params)
+
+	cmd := wkCmd + " " + params
+	res := exec.Command("bash", "-c", cmd)
+
+	// configure `Stdout` and `Stderr`
+	// res.Stdout = os.Stdout
+	// res.Stderr = os.Stdout
+
+	// run command
+	if err := res.Run(); err != nil {
+		log.Println("Error:", err)
+	}
+
+}
+
+func checkWkExists() string {
+	path, err := exec.LookPath("_wkhtmltopdf")
+	if err != nil {
+		log.Fatal("didn't find '_wkhtmltopdf' executable.\n")
+
+	} else {
+		log.Printf("'wkhtmltopdf' executable is in '%s'\n", path)
+	}
+	return path
+}
